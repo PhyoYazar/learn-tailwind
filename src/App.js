f7bc51f3d9cbdb290871be318db52cdce1927bf9
Phyo Yarzar <phyoyazar592@gmail.com>
2022-07-12 13:46:30 +0630

    Start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
