edc427ec089b1dd1a25dd60a4a610c37c1d0e791
Phyo Yarzar <phyoyazar592@gmail.com>
2022-07-17 21:53:34 +0630

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,10 +12,10 @@ const Hero = () => {
   return (
     <div
       name='home'
-      className='w-full md:h-screen bg-zinc-200 flex flex-col justify-between'
+      className='relative w-full md:h-screen bg-zinc-200 flex flex-col justify-between'
     >
       <div className='grid md:grid-cols-2 max-w-[1280px] m-auto'>
-        <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+        <div className='flex flex-col justify-center sm:items-center md:items-start w-full px-2 pb-8 pt-32 md:pt-8'>
           <p className='text-2xl'>Unique Sequencing & Production</p>
           <h1 className='py-3 text-5xl md:text-7xl font-bold'>
             Cloud Management
@@ -28,7 +28,7 @@ const Hero = () => {
           <img className='w-full' src={bgImage} alt='cloud img' />
         </div>
 
-        <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-20 md:bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+        <div className='absolute flex flex-col py-8 min-w-[250px] sm:min-w-[500px] md:min-w-[760px] bottom-[-10%] sm:bottom-5 md:bottom-[5%] mx-1 left:0 transform -translate-x-0 sm:left-1/2 sm:transform sm:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
           <p>Data Services</p>
           <div className='flex justify-between flex-wrap px-4'>
             <p className='flex px-4 py-2 text-slate-600'>
